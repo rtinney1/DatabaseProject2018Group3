6d343a35aeeeef8ebde050d176b93ebe51632596
rtinney1 <35842918+rtinney1@users.noreply.github.com>
2018-11-01 21:37:05 -0400

    Adds Test Values to DB Tables

---
+++ sql/insertIntoTables.sql
@@ -0,0 +1,31 @@
+INSERT INTO Sub_Plan VALUES (1, 9);
+INSERT INTO Sub_Plan VALUES (2, 7);
+INSERT INTO Sub_Plan VALUES (3, 5);
+INSERT INTO Sub_Plan VALUES (4, 3);
+
+INSERT INTO Address VALUES (0, 'WV', 'Fairmont', '123 Easy St', 26501);
+INSERT INTO Address VALUES (0, 'NY', 'New York City', 'Main St', 90121);
+INSERT INTO Address VALUES (0, 'TX', 'Houston', '192 Hick Rd', 12345);
+INSERT INTO Address VALUES (0, 'CA', 'Hollywood', 'Bollywood Ln', 90210);
+
+INSERT INTO Users VALUES('test1@name.com', 'John Smith', 'pass', '9991829145', 2, 0, 4);
+INSERT INTO Users VALUES('admin@name.com', 'Jane Smith', 'pass', '8888888888', 1, 1, 1);
+
+INSERT INTO Awards VALUES(0, 'Best Cartoon');
+INSERT INTO Awards VALUES(0, 'Worst Game');
+INSERT INTO Awards VALUES(0, 'Okayest Soundtrack');
+INSERT INTO Awards VALUES(0, 'Didnt Suck Entirely');
+
+INSERT INTO Cast_Member VALUES (0, 'Connie Webt', 3, 0);
+INSERT INTO Cast_Member VALUES (0, 'Chad Lit', 4, 1);
+
+INSERT INTO Entertainment VALUES(0, 'The Time Before Land', 'June 3 1980', 'Cartoon', 3, '1', 0, 'DVD', '1');
+INSERT INTO Entertainment VALUES(0, 'The Time Before Land 2', 'June 4 1980', 'Cartoon', 1, '0', 1, 'DVD', '1');
+INSERT INTO Entertainment VALUES(0, 'Timestop', 'Decemeber 21 2013', 'Mystery', 0, '3$4', 0, 'Xbox', '1.4');
+INSERT INTO Entertainment VALUES(0, 'Remember Remember', 'November 5 1995', 'Comedy', 3, '2', 0, 'PS2', '3.4');
+
+INSERT INTO Worked_In VALUES(1, 1);
+INSERT INTO Worked_In VALUES(2, 1);
+INSERT INTO Worked_In VALUES(2, 2);
+
+INSERT INTO Rent_History VALUES (0, 1, 'test1@name.com', '2008-09-09 12:12:12', 0);
