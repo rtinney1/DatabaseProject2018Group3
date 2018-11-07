b7decbadb69e6a2b22b4ee36f2eed1d3a473cb04
rtinney1 <35842918+rtinney1@users.noreply.github.com>
2018-11-07 06:29:19 -0500

    Comments, Transactions, and Fixes
    Added comments to all files to make things easier to find and understand what they are doing. Added all of the needed transactions. I tested them, but they should be tested again. Found a couple of errors with some of the queries, especially in Entertainment with getting the sequals and some of the searches

--- java/AddEntertainmentException.java
+++ java/AddEntertainmentException.java
@@ -1,3 +1,13 @@
+/*AddEntertainmentException.java
+ * Created by: Randi Tinney
+ * Created On: Oct 24 2018
+ * Updated On: Oct 24 2018
+ * Description: AddEntertainmentException.java is to be used when adding
+ * 		an Entertainment object to the database. 
+ */
+
+package application;
+
 
 public class AddEntertainmentException extends Exception
 {
@@ -6,3 +16,4 @@ public class AddEntertainmentException extends Exception
 	    super(message);
 	  }
 }
+
